5ba8e7626fa407a321605a392391b5f24ce37911
F. Talha Altinel <talhaaltinel@hotmail.com>
2022-02-13 21:33:59 +0000

    drawing board

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -7,6 +7,7 @@ import com.soywiz.korim.format.*
 import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.degrees
 import com.soywiz.korma.geom.shape.Shape2d
+import com.soywiz.korma.geom.vector.roundRect
 import com.soywiz.korma.interpolation.Easing
 
 suspend fun main() = Korge(width = 480, height = 640, title="linije", bgcolor = Colors["#641398"]) {
@@ -36,4 +37,25 @@ suspend fun main() = Korge(width = 480, height = 640, title="linije", bgcolor = 
 	}*/
 
 	circle(20.0, Colors.YELLOW, Colors.GREEN, 10.0).xy(100, 100)
+
+	val cellSize = views.virtualWidth / 15.0 // for 8 cells horizontally
+	val fieldSize = (cellSize * 15.0 + views.virtualWidth % 15) - 100
+	val leftIndent = 50.0
+	val topIndent = 150.0
+
+	roundRect(fieldSize, fieldSize+ (14+ cellSize), 5.0, fill = Colors.YELLOW).apply {
+		position(leftIndent, topIndent)
+	}
+
+	graphics {
+		position(leftIndent, topIndent)
+		fill(Colors.BLUE) {
+			for (i in 0..7) {
+				for (j in 0..8) {
+					roundRect(14 + (14 + cellSize) * i, 14 + (14 + cellSize) * j, cellSize, cellSize, 5.0)
+				}
+			}
+		}
+	}
+
 }
